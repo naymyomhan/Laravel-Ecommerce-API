428340896b9aa0688ea12d6cb9ff971cb0fb070f
Nay Myo Mhan <naymyomhan@gmail.com>
2023-12-25 03:50:17 +0700

    update

--- app/Http/Controllers/User/UserAuthController.php
+++ app/Http/Controllers/User/UserAuthController.php
@@ -12,6 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -23,9 +24,7 @@ class UserAuthController extends Controller
     {
         DB::beginTransaction();
         try {
-
             $verification_token = random_int(100000, 999999);
-
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
@@ -76,8 +75,52 @@ class UserAuthController extends Controller
         }
     }
 
+    public function requestVerificationToken()
+    {
+        DB::beginTransaction();
+        try {
+            $user = Auth::user();
+            if (!$user) {
+                return $this->fail("unauthorized", 401);
+            }
+
+            if ($user->email_verified_at !== null && $user->email_verified_at !== "") {
+                return $this->fail("Already verify", 400);
+            }
+
+            $token_request_at = $user->token_request_at;
+            if ($token_request_at !== null) {
+                $token_request_date = Carbon::createFromFormat('Y-m-d H:i:s', $token_request_at)->format('Y-m-d');
+                $today = Carbon::now()->format('Y-m-d');
+                if ($token_request_date == $today) {
+                    $today_token_request_count = $user->token_request_count;
+                    if ($today_token_request_count >= 3) {
+                        return $this->fail("too many attempt, please try tomorrow", 429);
+                    }
+                } else {
+                    $user->token_request_count = 0;
+                    /** @var \App\Models\User $user **/
+                    $user->save();
+                }
+            }
+
+            $random_otp = random_int(100000, 999999);
+            $user->verification_token = Crypt::encryptString($random_otp);
+            $user->token_request_at = Carbon::now();
+            /** @var \App\Models\User $user **/
+            $user->increment('token_request_count', 1);
+            $user->save();
+            DB::commit();
+            return $this->success("Send OTP successful", $random_otp); //TODO::remove code
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return $this->fail($th->getMessage() ? $th->getMessage() : "server error", 500);
+        }
+    }
+
     public function verifyEmail(UserEmailVerifyRequest $request)
     {
+        /** @var \App\Models\User $user **/
         $user = Auth::user();
         if (!$user) {
             return $this->fail("unauthorized", 401);
@@ -87,22 +130,43 @@ class UserAuthController extends Controller
             return $this->fail("Already verified", 400);
         }
 
-        if ($user->verification_token !== $request->verification_token) {
-            return $this->fail("Verification fail", 401);
-        }
-
         DB::beginTransaction();
         try {
-            $user->email_verified_at = Carbon::now();
-            /** @var \App\Models\User $user **/
-            $user->save();
+            $verifyAttemptAt = $user->verify_attempt_at;
 
-            $data = [
-                "verified" => true,
-            ];
+            if ($verifyAttemptAt !== null) {
+                $verifyAttemptTime = Carbon::createFromFormat('Y-m-d H:i:s', $verifyAttemptAt)->format('YmdHis');
+                $currentTime = Carbon::now()->format('YmdHis');
 
-            DB::commit();
-            return $this->success("Verify email successful", $data);
+                if ($currentTime - $verifyAttemptTime < 60) {
+                    $todayVerifyAttemptCount = $user->verify_attempt_count;
+                    if ($todayVerifyAttemptCount >= 5) {
+                        return $this->fail("Too many attempt, please try again later", 429);
+                    }
+                } else {
+                    $user->verify_attempt_count = 0;
+                    $user->save();
+                }
+            }
+
+
+
+            $decryptedOTP = Crypt::decryptString($user->verification_token);
+            if ($decryptedOTP !== $request->verification_token) {
+                $user->verify_attempt_at = Carbon::now();
+                $user->increment('verify_attempt_count', 1);
+                $user->save();
+                DB::commit();
+                return $this->fail("Verification fail", 401);
+            } else {
+                $user->email_verified_at = Carbon::now();
+                $user->verify_attempt_at = null;
+                $user->verify_attempt_count = 0;
+                $user->save();
+                $data = ["verified" => true];
+                DB::commit();
+                return $this->success("Verify email successful", $data);
+            }
         } catch (\Throwable $th) {
             DB::rollBack();
             return $this->fail("Server Error : " . $th);
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\User\UserAuthController;
 use App\Http\Controllers\User\UserProductController;
+use App\Http\Controllers\User\VerificationController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 // User
 Route::post('/user/register', [UserAuthController::class, 'register']);
 Route::post('/user/login', [UserAuthController::class, 'login']);
-Route::post('/user/email/verify', [UserAuthController::class, 'verifyEmail'])->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/user/verification_token/request', [UserAuthController::class, 'requestVerificationToken']);
+    Route::post('/user/email/verify', [UserAuthController::class, 'verifyEmail']);
+});
 
 Route::group(['middleware' => ['auth:sanctum', 'email.verified']], function () {
     Route::get('/products', [UserProductController::class, 'getAllProducts']);
